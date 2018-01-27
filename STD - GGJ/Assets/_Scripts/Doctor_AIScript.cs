907ad0fc4a08ce619a60d932b0c3ab6e4cb872b7
Kristian <kristianmischke@gmail.com>
2018-01-27 10:58:24 -0500

    merge?

---
+++ STD - GGJ/Assets/_Scripts/Doctor_AIScript.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Doctor_AIScript : MonoBehaviour {
+    public GameObject worldObject;
+    NPCSpawner worldSpawner;
+    public GameObject targetNPC;
+
+    public static float MIN_WAIT = 0f;
+    public static float MAX_WAIT = 4f;
+
+    Vector3 target;
+    Vector3 direction;
+    Vector3 heading;
+    Vector3 targetNPCWaypoint;
+    float distance;
+    Stack<Vector3> waypoints;
+
+    float wait = 0;
+    bool walking = false;
+
+    LineRenderer path;
+
+    // Use this for initialization
+    void Start () {
+        if (worldObject == null)
+        {
+            worldObject = GameObject.Find("_Scripts");
+        }
+
+        if (worldSpawner == null)
+        {
+            worldSpawner = worldObject.GetComponent<NPCSpawner>();
+        }
+
+        path = gameObject.AddComponent<LineRenderer>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+
+        gameObject.transform.Translate(direction * Time.deltaTime * 5);
+
+        UpdateMovement();
+
+        if (distance < 1 || target == null)
+        {
+            walking = false;
+            int x = Random.Range(0, worldSpawner.npcList.Length-1);
+
+            targetNPC = worldSpawner.npcList[x];
+        }
+
+        
+
+        target = targetNPC.transform.position;
+
+        UpdateMovement();
+
+        int counter = 0;
+        RaycastHit hits;
+
+        while (Physics.Raycast(gameObject.transform.position, direction, out hits, distance))
+        {
+            if (counter == 0)
+            {
+                target = hits.transform.position;
+            }
+
+            else
+            {
+                target += Vector3.Lerp(direction, hits.normal, 0.5f);
+
+            }
+            UpdateMovement();
+            counter++;
+        }
+
+        path.SetPositions(new Vector3[] { gameObject.transform.position + Vector3.back, target + Vector3.back });
+
+
+
+        
+
+    }
+
+
+
+    private void UpdateMovement()
+    {
+
+        heading = target - gameObject.transform.position;
+        distance = heading.magnitude;
+        direction = heading / distance;
+
+    }
+}
+
--- STD - GGJ/Assets/_Scripts/NPCSpawner.cs
+++ STD - GGJ/Assets/_Scripts/NPCSpawner.cs
@@ -8,29 +8,40 @@ public class NPCSpawner : MonoBehaviour {
 
     public GameObject npcObject;
     public int numNPCs = 100;
+    public GameObject doctorObject;
+    public const int DOCTOR_MAX = 2;
 
     public float worldMinX = -30;
     public float worldMaxX = 30;
     public float worldMinY = -30;
     public float worldMaxY = 30;
+    public GameObject[] npcList = new GameObject[100];
 
 
 	// Use this for initialization
 	void Start () {
         SpawnNPCs();
+        SpawnDoctors();
 	}
 
 
-
+    
     public void SpawnNPCs() {
 
         for (int i = 0; i < numNPCs; i++) {
 
             GameObject person = Instantiate(npcObject, new Vector3(Random.Range(worldMinX, worldMaxX), Random.Range(worldMinY, worldMaxY)), Quaternion.identity, objectContainer.transform);
             person.GetComponent<AIWalk>().worldObject = gameObject;
-
+            npcList[i] = person;
         }
 
     }
+
+    public void SpawnDoctors()
+    {
+        GameObject person = Instantiate(npcObject, new Vector3(Random.Range(worldMinX, worldMaxX), Random.Range(worldMinY, worldMaxY)), Quaternion.identity, objectContainer.transform);
+        person.GetComponent<Doctor_AIScript>().worldObject = gameObject;
+    }
+
 
 }
